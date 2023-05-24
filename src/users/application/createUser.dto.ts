273252c439118861d6511121a17cfcbe2636a3cb
daniel.granados <daniel.granados@sofka.com.co>
2023-05-23 19:51:46 -0500

    add: create and update user swagger

--- src/users/application/createUser.dto.ts
+++ src/users/application/createUser.dto.ts
@@ -1,30 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsEmail, IsIn } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @ApiProperty({ enum: ['CC', 'CE', 'TI', 'PPN', 'NIT'] })
   id_type: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String })
   id_number: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'John' })
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'Doe' })
   lastName: string;
 
   @IsEmail()
+  @ApiProperty({ type: String, example: 'jhondoe@email.com' })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String, example: '123456789' })
   phone: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'Calle 123' })
   address: string;
 
   @IsIn(['admin', 'accountant', 'sofware_consultant'])
+  @ApiProperty({ enum: ['admin', 'accountant', 'sofware_consultant'] })
   rol?: string;
 
   @IsNotEmpty()
+  @ApiProperty({ type: Boolean, example: 'true/false' })
   state: boolean;
 }
--- src/users/application/updateUser.dto.ts
+++ src/users/application/updateUser.dto.ts
@@ -1,7 +1,9 @@
 import { IsNotEmpty } from 'class-validator';
 import { CreateUserDto } from './createUser.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto extends CreateUserDto {
   @IsNotEmpty()
+  @ApiProperty({ type: Number, required: true })
   id: number;
 }
