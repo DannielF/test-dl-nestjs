9dea81eea732ff8d9bc7a2d85587fb9e8c3c2b10
daniel.granados <daniel.granados@sofka.com.co>
2023-05-23 19:48:25 -0500

    add: user controller swagger

--- src/users/infrastructure/user.controller.ts
+++ src/users/infrastructure/user.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Logger,
   Param,
@@ -11,54 +12,79 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from '../application/createUser.dto';
 import { UpdateUserDto } from '../application/updateUser.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @Controller({ path: 'users', version: '1' })
+@ApiTags('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   private readonly logger = new Logger(UserController.name);
 
   @Get()
+  @ApiOperation({ summary: 'Get all users' })
   async getAll() {
     this.logger.log('Get all users');
-    return this.userService.getAll();
+    return await this.userService.getAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get user by id' })
+  @ApiParam({ name: 'id', type: Number })
   async getUserById(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.getUserById(id);
+    return await this.userService.getUserById(id);
   }
 
   @Get('get-by/:type/:number')
+  @ApiOperation({ summary: 'Get user by type and number document' })
+  @ApiParam({ name: 'type', type: String })
+  @ApiParam({ name: 'number', type: String })
   async getUserByTypeAndNumberDocument(
     @Param('type') type: string,
     @Param('number') number: string,
   ) {
-    return this.userService.getUserByTypeAndNumberDocument(type, number);
+    return await this.userService.getUserByTypeAndNumberDocument(type, number);
   }
 
   @Get('get-by-email/:email')
+  @ApiOperation({ summary: 'Get user by email' })
+  @ApiParam({ name: 'email', type: String })
   async getUserByEmail(@Param('email') email: string) {
-    return this.userService.getUserByEmail(email);
+    return await this.userService.getUserByEmail(email);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create user' })
+  @ApiBody({ type: CreateUserDto, required: true })
   async createUser(@Body() user: CreateUserDto) {
-    return this.userService.createUser(user);
+    return await this.userService.createUser(user);
   }
 
   @Post('add-rol/:id/:rol')
+  @ApiOperation({ summary: 'Add rol to user' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiParam({ name: 'rol', type: String })
   async addRolToUser(
     @Param('id', ParseIntPipe) id: number,
     @Param('rol') rol: string,
   ) {
-    return this.userService.addRolToUser(id, rol);
+    return await this.userService.addRolToUser(id, rol);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update user' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiBody({ type: UpdateUserDto, required: true })
   async updateUser(
     @Param('id', ParseIntPipe) id: number,
     @Body() user: UpdateUserDto,
   ) {
-    return this.userService.updateUser(id, user);
+    return await this.userService.updateUser(id, user);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete user' })
+  @ApiParam({ name: 'id', type: Number })
+  async deleteUser(id: number) {
+    return await this.userService.deleteUser(id);
   }
 }
